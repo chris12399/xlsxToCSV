e440f77d20a3f7cfcdd21506a9670830ea30b578
user_ubuntu <maxpower12399@gmail.com>
2021-09-11 10:04:50 +0800

    功能運作正常
    update xlsxToCSV.java

--- xlsxToCSV/src/main/java/com/company/xlsxToCSV.java
+++ xlsxToCSV/src/main/java/com/company/xlsxToCSV.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.util.Iterator;
 
 import org.apache.commons.io.FilenameUtils;
@@ -20,15 +17,18 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class xlsxToCSV {
 
-
    static void readXlsx(File inputFile, File outputFile) {
        StringBuffer data = new StringBuffer();
 
        try{
            FileInputStream fis = new FileInputStream(inputFile);
            FileOutputStream fos = new FileOutputStream(outputFile);
+//           OutputStreamWriter osw = new OutputStreamWriter(outputFile);
 
+           //read file
            Workbook workbook = null;
+           //write file
+           Workbook workbook1 = null;
 
            String ext  = FilenameUtils.getExtension(inputFile.toString());
 
@@ -37,10 +37,46 @@ public class xlsxToCSV {
            }else if(ext.equalsIgnoreCase("xls")){
                workbook = new HSSFWorkbook(fis);
            }
+           int numberOfSheets = workbook.getNumberOfSheets();
+            Row row;
+            Cell cell1, cell2;
+            Sheet sheet = workbook.getSheetAt(0);
+            String[] titles = new String[] {"login_name","userpassword","name","depid","email"};
+
+           for(String title : titles){
+               fos.write(title.getBytes());
+               fos.write(",".getBytes());
+           }
+           fos.write("\r\n".getBytes());
+
+           for(int i = 1; i <= sheet.getLastRowNum(); i++) {
+               row = sheet.getRow(i);
+//               System.out.println(row.toString());
+               cell1 = row.getCell(1);                //teacher's name
+               cell2 = row.getCell(6);                //e-mail
 
 
+               //trim()
 
-
+               //login_name
+               fos.write(cell2.toString().trim().getBytes());
+               fos.write("@gm.kl.edu.tw".getBytes());
+               fos.write(",".getBytes());
+               //userpassword
+               fos.write("password".getBytes());
+               fos.write(",".getBytes());
+               //name
+               fos.write(cell1.toString().getBytes());
+               fos.write(",".getBytes());
+               //depid
+               fos.write("100".getBytes());
+               fos.write(",".getBytes());
+               //email
+               fos.write(cell2.toString().getBytes());
+               fos.write("@gm.kl.edu.tw".getBytes());
+               fos.write(",".getBytes());
+               fos.write("\r\n".getBytes());
+           }
 
        } catch(Exception e) {
            System.out.println("*****read excel*****");
@@ -48,7 +84,7 @@ public class xlsxToCSV {
        }
    }
 
-
+//************************************************************************
 
     static void xlsx(File inputFile, File outputFile) {
         StringBuffer data = new StringBuffer();
@@ -73,7 +109,6 @@ public class xlsxToCSV {
             Row row;
             Cell cell;
 
-
             for (int i = 0; i < numberOfSheets; i++) {
                 Sheet sheet = workbook.getSheetAt(0);
 
@@ -86,15 +121,7 @@ public class xlsxToCSV {
                     // For each row, iterate through each columns
                     Iterator<Cell> cellIterator = row.cellIterator();
                     while (cellIterator.hasNext()) {
-
-                        cell = cellIterator.next();
-
-
-//                        String str;
-//                        str = (String) cellIterator.next().toString().subSequence(0,1);
-//                        System.out.println(str);
-
-
+                      cell = cellIterator.next();
 
                         switch (cell.getCellType()) {
                             case BOOLEAN:
@@ -136,10 +163,12 @@ public class xlsxToCSV {
         // writing excel data to csv
         File outputFile = new File("/home/user/Downloads/govMail/七堵國小.csv");  //provide your path
         //work space
-        xlsx(inputFile, outputFile);
+        //xlsx(inputFile, outputFile);
+        readXlsx(inputFile, outputFile);
+        System.out.println("this is readXlsx ~~~~~~~~~~");
 
-        System.out.println("Conversion of " + inputFile + " to flat file: "
-                + outputFile + " is completed");
+//        System.out.println("Conversion of " + inputFile + " to flat file: "
+//                + outputFile + " is completed");
     }
 
 }

f5195ac3e0cc6175269f0138814ceec034ddc91e
user_ubuntu <maxpower12399@gmail.com>
2021-09-11 11:03:27 +0800

    修正空白及刪除範例

--- src/main/java/com/company/xlsxToCSV.java
+++ src/main/java/com/company/xlsxToCSV.java
@@ -72,7 +72,7 @@ public class xlsxToCSV {
                fos.write("100".getBytes());
                fos.write(",".getBytes());
                //email
-               fos.write(cell2.toString().getBytes());
+               fos.write(cell2.toString().trim().getBytes());
                fos.write("@gm.kl.edu.tw".getBytes());
                fos.write(",".getBytes());
                fos.write("\r\n".getBytes());
@@ -86,89 +86,18 @@ public class xlsxToCSV {
 
 //************************************************************************
 
-    static void xlsx(File inputFile, File outputFile) {
-        StringBuffer data = new StringBuffer();
-
-
-        try {
-            FileOutputStream fos = new FileOutputStream(outputFile);
-            FileInputStream fis = new FileInputStream(inputFile);
-
-            Workbook workbook = null;
-
-            String ext  = FilenameUtils.getExtension(inputFile.toString());
-
-            if(ext.equalsIgnoreCase("xlsx")) {
-                workbook = new XSSFWorkbook(fis);
-            }else if(ext.equalsIgnoreCase("xls")){
-                workbook = new HSSFWorkbook(fis);
-            }
-            // get first sheet from the workbook
-
-            int numberOfSheets = workbook.getNumberOfSheets();
-            Row row;
-            Cell cell;
-
-            for (int i = 0; i < numberOfSheets; i++) {
-                Sheet sheet = workbook.getSheetAt(0);
-
-
-                Iterator<Row> rowIterator = sheet.iterator();
-
-                while (rowIterator.hasNext()) {
-                    row = rowIterator.next();
-
-                    // For each row, iterate through each columns
-                    Iterator<Cell> cellIterator = row.cellIterator();
-                    while (cellIterator.hasNext()) {
-                      cell = cellIterator.next();
-
-                        switch (cell.getCellType()) {
-                            case BOOLEAN:
-                                data.append(cell.getBooleanCellValue() + ",");
-
-                                break;
-                            case NUMERIC:
-                                data.append(cell.getNumericCellValue() + ",");
-
-                                break;
-                            case STRING:
-                                data.append(cell.getStringCellValue() + ",");
-//                                System.out.println(cell.getStringCellValue());
-                                break;
-
-                            case BLANK:
-                                data.append("" + ",");
-                                break;
-                            default:
-                                data.append(cell + ",");
-
-                        }
-                    }
-                    data.append('\n'); // appending new line after each row
-                }
-            }
-            fos.write(data.toString().getBytes());
-            fos.close();
-
-        } catch(Exception ioe) {
-            ioe.printStackTrace();
-        }
-
-    }
 
     public static void main(String[] args) {
         // reading file from desktop
         File inputFile = new File("/home/user/Downloads/govMail/七堵國小.xlsx"); //provide your path
         // writing excel data to csv
-        File outputFile = new File("/home/user/Downloads/govMail/七堵國小.csv");  //provide your path
+        File outputFile = new File("/home/user/Downloads/govMail/importCSV/七堵國小.csv");  //provide your path
         //work space
         //xlsx(inputFile, outputFile);
         readXlsx(inputFile, outputFile);
-        System.out.println("this is readXlsx ~~~~~~~~~~");
 
-//        System.out.println("Conversion of " + inputFile + " to flat file: "
-//                + outputFile + " is completed");
+        System.out.println("Conversion of " + inputFile + " to flat file: "
+                + outputFile + " is completed");
     }
 
 }

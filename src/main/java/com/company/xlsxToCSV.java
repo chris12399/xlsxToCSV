973078a8a907a5a69c0a9ffcb0454eb359a63ee8
user_ubuntu <maxpower12399@gmail.com>
2021-09-15 10:16:13 +0800

    修改不讀取表格中空值的情形

--- src/main/java/com/company/xlsxToCSV.java
+++ src/main/java/com/company/xlsxToCSV.java
@@ -8,10 +8,12 @@ import org.apache.commons.io.FilenameUtils;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 
+import org.apache.poi.util.StringUtil;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import sun.awt.X11.XSystemTrayPeer;
 
 public class xlsxToCSV {
 
@@ -38,7 +40,10 @@ public class xlsxToCSV {
            int numberOfSheets = workbook.getNumberOfSheets();
 
             Row row;
-            Cell cell1, cell2;
+            Cell cell1 = null;
+            Cell cell2 = null;
+            Cell cell3 = null;
+
             Sheet sheet = workbook.getSheetAt(0);
             String[] titles = new String[] {"login_name","userpassword","name","depid","email"};
 
@@ -51,31 +56,41 @@ public class xlsxToCSV {
 
            for(int i = 1; i <= sheet.getLastRowNum(); i++) {
                row = sheet.getRow(i);
-//               System.out.println(row.toString());
-               cell1 = row.getCell(1);                //teacher's name
-               cell2 = row.getCell(6);                //e-mail
 
-               //trim()
+               cell3 = row.getCell(0);
+               String blank = cell3.toString();
+//               if(!"".equals(cell1) || !"".equals(cell2)) {}
+//               Iterator<Cell> cellIterator = row.cellIterator();
+//               while(cellIterator.hasNext()) {}
+//               row != null &&
+               if(!blank.equals("")) {
+                   cell1 = row.getCell(1);                //teacher's name
+                   cell2 = row.getCell(6);                //e-mail
 
-               //login_name
-               bw.write(cell2.toString().trim());
-               bw.write("@mail.klcg.gov.tw");
-               bw.write(",");
-               //userpassword
-               bw.write("password");
-               bw.write(",");
-               //name
-               bw.write(cell1.toString());
-               bw.write(",");
-               //depid
-               bw.write("173626");         //記得更換學校代碼
-               bw.write(",");
-               //email
-               bw.write(cell2.toString().trim());
-               bw.write("@mail.klcg.gov.tw");
-               bw.write(",");
-               bw.write("\r\n" );
+                   //trim()
+
+                   //login_name
+                   bw.write(cell2.toString().trim());
+                   bw.write("@mail.klcg.gov.tw");
+                   bw.write(",");
+                   //userpassword
+                   bw.write("password");
+                   bw.write(",");
+                   //name
+                   bw.write(cell1.toString());
+                   bw.write(",");
+                   //depid
+                   bw.write("173508");         //記得更換學校代碼
+                   bw.write(",");
+                   //email
+                   bw.write(cell2.toString().trim());
+                   bw.write("@mail.klcg.gov.tw");
+                   bw.write(",");
+                   bw.write("\r\n" );
+               } else { break; }
+
            }
+           bw.flush();
            bw.close();
            osw.close();
 
@@ -90,9 +105,9 @@ public class xlsxToCSV {
 
     public static void main(String[] args) {
         // reading file from desktop
-        File inputFile = new File("/home/user/Downloads/govMail/七堵國小.xlsx"); //provide your path
+        File inputFile = new File("/home/user/Downloads/govMail/南榮國中.xlsx"); //provide your path
         // writing excel data to csv
-        File outputFile = new File("/home/user/Downloads/govMail/importCSV/七堵國小.csv");  //provide your path
+        File outputFile = new File("/home/user/Downloads/govMail/importCSV/南榮國中.csv");  //provide your path
         //work space
         //xlsx(inputFile, outputFile);
         readXlsx(inputFile, outputFile);
